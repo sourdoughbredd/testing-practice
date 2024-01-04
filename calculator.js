8526c2a8661b24b36954fdf49f1b36dbde0dcce7
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-04 09:40:29 -0500

    Complete and test calculator methods

--- calculator.js
+++ calculator.js
@@ -1,8 +1,16 @@
 export { calculator };
 
 const calculator = {
-  add: function (a, b) {},
-  subtract: function (a, b) {},
-  multiply: function (a, b) {},
-  divide: function (a, b) {},
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
 };
