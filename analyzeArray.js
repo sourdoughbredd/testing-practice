fadf01ea6c465eb50ac0e2c84a39469c8684f988
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-04 11:17:52 -0500

    Finish and test analyzeArray function

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,5 +1,24 @@
 export { analyzeArray };
 
 function analyzeArray(array) {
-  return {};
+  if (!array.every((element) => typeof element === "number")) {
+    throw new Error("Array contains non-numbers!");
+  }
+
+  const length = array.length;
+
+  const average =
+    length == 0 ? null : array.reduce((prev, curr) => prev + curr, 0) / length;
+
+  const min =
+    length == 0
+      ? null
+      : array.reduce((prev, curr) => (curr < prev ? curr : prev), Infinity);
+
+  const max =
+    length == 0
+      ? null
+      : array.reduce((prev, curr) => (curr > prev ? curr : prev), -Infinity);
+
+  return { average, min, max, length };
 }
